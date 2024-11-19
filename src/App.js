1188319563626686ff9216ac117f49c8278c6784
uliashem <shemiakinau@gmail.com>
2024-11-19 13:04:34 +0000

    New project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import image from "./background.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [quote, setQuote] = useState("");
+
+  const getQuote = async () => {
+    const response = await fetch(`https://bored.api.lewagon.com/api/activity`);
+    const data = await response.json();
+    setQuote(data.activity);
+  }
+
+  useEffect(() => {
+    getQuote();
+
+    }, [])
+
+
+  return (<div className='App'>
+
+    <div className='container'>
+      <img src={image} className="img" alt='Background'/>
     </div>
+
+    <div className="container">
+      <h1>Let's find you something to do!</h1>
+    </div>
+
+    <div className="container">
+       <p>{quote}</p>
+    </div>
+
+    <div className="container">
+      <button onClick={getQuote}>Try new activity</button>
+    </div>
+      
+  </div>
   );
 }
 
